e01892e0853df597c714009075051515c7ef3f60
Cristobal Dominguez <cristobald@gmail.com>
2017-10-20 20:49:39 -0300

    Moves Components to a New Folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import NewsFeed from './NewsFeed';
+import NewsFeed from './components/NewsFeed';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<NewsFeed />, document.getElementById('App'));
